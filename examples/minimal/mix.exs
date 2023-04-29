f697f90e780a4eda1a1cd722fb421a2492e1bfe5
Sindri P. Ingimundarson <sindripetur@gmail.com>
2023-04-29 20:57:24 +0200

    Update example with remote dependency

--- examples/minimal/mix.exs
+++ examples/minimal/mix.exs
@@ -23,9 +23,7 @@ defmodule Minimal.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:scratch_release, path: "../../../scratch_release"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:scratch_release, "~> 0.1.0"}
     ]
   end
 
