9f71a5906349644589db26a444851bc942e42015
Sindri P. Ingimundarson <sindripetur@gmail.com>
2023-04-29 18:55:00 +0200

    prepare hex release

--- mix.exs
+++ mix.exs
@@ -1,15 +1,20 @@
 defmodule ScratchRelease.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/sindrip/scratch_release"
+
   def project do
     [
       app: :scratch_release,
       version: "0.1.0",
       elixir: "~> 1.14",
-      start_permanent: Mix.env() == :prod,
       deps: deps(),
+      package: package(),
       preferred_cli_env: [docs: :docs],
-      name: "ScratchRelease"
+      docs: [extras: "README.md"],
+      name: "ScratchRelease",
+      description: "Package a release that can run in a scratch docker image",
+      source_url: @source_url
     ]
   end
 
@@ -22,6 +27,14 @@ defmodule ScratchRelease.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.27", only: :docs, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
     ]
   end
 end
